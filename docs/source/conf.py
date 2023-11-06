549d841423684fb658027d77ccc7d5d4a0288656
Jindrich Luza <jluza@redhat.com>
2023-11-06 12:35:20 +0100

    Release v0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'pubtools-sign'
 copyright = '2023, Author'
 author = 'Red Hat'
-release = '0.1.0'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
